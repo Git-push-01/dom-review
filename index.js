9a9d4116b3af22029f8b7d8859eff2cd7ce19631
Jane Costa <yberdysh@gmail.com>
2018-10-11 15:54:13 -0400

    group 1 review

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const buttonArea = document.getElementById('buttons')
-  const multTable = document.getElementById('mult-table')
-  createButtons()
-  generateMultiplicationTable(1)
-
-  let buttons = document.querySelectorAll('.multiplier')
-  buttons.forEach(button => {
-    console.log(button)
-    button.addEventListener("click", (event) => {
-      console.log("Clicked")
-      let number = parseInt(event.target.innerText)
-      generateMultiplicationTable(number)
-    })
-  })
-
-  // debugger;
-
-  function createButtons(){
-    for (let i = 0; i <= 10; i++){
-      buttonArea.innerHTML += `<button class="multiplier">${i}</button>`
-      // let newButton = document.createElement('button');
-      // newButton.className = "multiplier"
-      // newButton.innerText = `${i}`
-      // buttonArea.append(newButton)
-    }
-  }
-
-    function generateMultiplicationTable(num){
-      multTable.innerHTML = '';
-      for (let i = 1; i <= 10; i++){
-        let newRow = `<tr>
-        <td>
-          ${num} X ${i} = ${num*i}
-        </td>
-        </tr>`
-        multTable.innerHTML += newRow;
-      }
-    }
+	const buttonArea = document.querySelector('#buttons')
+	const tableArea = document.querySelector('#mult-table')
+	createButtons()
+	createTable()
 
 
+	function createButtons() {
+		for (let i = 1; i <= 10; i++) {
+			const newButton = document.createElement('button')
+			newButton.innerText = i
+			newButton.className = "multiplier"
+			newButton.addEventListener("click", (event) => {
+				let number = parseInt(event.target.innerText)
+				createTable(number)
+			})
+			buttonArea.append(newButton)
+		}
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	function createTable(number = 1) {
+		tableArea.innerHTML = ''
+		for (let i = 1; i <= 10; i++) {
+			// const newRow = document.createElement('tr')
+			tableArea.innerHTML += `<tr><td>
+      ${number} X ${i} = ${i*number}
+    </td></tr>`
+		}
+	}
 })
